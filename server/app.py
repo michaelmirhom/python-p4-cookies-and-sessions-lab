c54c1f9f0937da27a4620c12a1564d574f7b4e50
michael mirhom <micharby73@gmail.com>
2023-09-20 16:14:21 -0500

    ompletyed

--- server/app.py
+++ server/app.py
@@ -4,6 +4,8 @@ from flask import Flask, make_response, jsonify, session
 from flask_migrate import Migrate
 
 from models import db, Article, User
+import warnings
+
 
 app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
@@ -22,13 +24,34 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
-
+ 
     pass
 
 @app.route('/articles/<int:id>')
 def show_article(id):
+   
+    session['page_views'] = session.get('page_views', 0) + 1
 
-    pass
+   
+    if session['page_views'] > 3:
+        return {'message': 'Maximum pageview limit reached'}, 401
+
+    
+    article = Article.query.get(id)
+    if not article:
+        return {'message': 'Article not found'}, 404
+
+    return jsonify({
+        'title': article.title,
+        'content': article.content,
+        'author_id': article.user_id,
+          'author': article.author,
+          'preview': article.preview,
+          'minutes_to_read': article.minutes_to_read, 
+           'date': article.date
+    })
+
 
 if __name__ == '__main__':
     app.run(port=5555)
+
